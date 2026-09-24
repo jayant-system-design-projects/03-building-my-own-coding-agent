64cd4f6654201a8bdae56d1c65cc6d038ae632e2
jayant <jayantarsode234@gmail.com>
2026-09-24 23:48:45 +0530

    Changes config.py

--- app/config.py
+++ app/config.py
@@ -6,11 +6,10 @@ load_dotenv()
 
 class Config:
     # These are api key,model name and base url for llm you can use you own
-    API_KEY = os.getenv("OPENROUTER_API_KEY")
-    BASE_URL = os.getenv("OPENROUTER_BASE_URL", "https://openrouter.ai/api/v1")
-    MODEL_NAME = os.getenv(
-        "OPENROUTER_MODEL_NAME", "nvidia/nemotron-3-ultra-550b-a55b:free"
-    )
+    # You can get free api keys from openrouter or nvidia https://build.nvidia.com/models
+    API_KEY = os.getenv("MODEL_API_KEY")
+    BASE_URL = os.getenv("MODEL_BASE_URL", "https://openrouter.ai/api/v1")
+    MODEL_NAME = os.getenv("MODEL_NAME", "nvidia/nemotron-3-ultra-550b-a55b:free")
 
     MAX_ITERATIONS_LOW_MODE = int(os.getenv("MAX_ITERATIONS_LOW_MODE", 5))
     MAX_ITERATIONS_MEDIUM_MODE = int(os.getenv("MAX_ITERATIONS_MEDIUM_MODE", 5))
